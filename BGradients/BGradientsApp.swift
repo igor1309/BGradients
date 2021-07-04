dc5f2b905ee501b79b5601d7d60d2a4408aa15b3
Igor Malyarov <igor1309@gmail.com>
2021-07-04 12:57:00 +0300

    Initial Commit

---
+++ BGradients/BGradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGradientsApp.swift
+//  BGradients
+//
+//  Created by Igor Malyarov on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BGradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
